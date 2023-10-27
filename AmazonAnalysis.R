1f8ccd0a702f4049d82c96054547d1033da5e524
Cameron Slaugh <cameronbyufan@gmail.com>
2023-10-27 15:45:24 -0600

    svm

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -70,155 +70,155 @@ format_and_write(log_predictions, "log_preds.csv")
 
 # Penalized Logistic Regression ------------------------------------------
 
-pog_mod <- logistic_reg(mixture=tune(), penalty=tune()) %>%
-  set_engine("glmnet")
-
-pog_workflow <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(pog_mod)
-
-tuning_grid <- grid_regular(penalty(),
-                            mixture(),
-                            levels = 4)
-
-folds <- vfold_cv(rawdata, v = 10, repeats=1)
-
-cl <- makePSOCKcluster(10)
-registerDoParallel(cl)
-CV_results <- pog_workflow %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics=metric_set(roc_auc))
-stopCluster(cl)
-
-bestTune <- CV_results %>%
-  select_best("roc_auc")
-
-final_pog_wf <-
-  pog_workflow %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=rawdata)
-
-
-pog_predictions <- final_pog_wf %>%
-  predict(new_data = test_input, type="prob")
-
-format_and_write(pog_predictions, "pog_preds.csv")
+# pog_mod <- logistic_reg(mixture=tune(), penalty=tune()) %>%
+#   set_engine("glmnet")
+# 
+# pog_workflow <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(pog_mod)
+# 
+# tuning_grid <- grid_regular(penalty(),
+#                             mixture(),
+#                             levels = 4)
+# 
+# folds <- vfold_cv(rawdata, v = 10, repeats=1)
+# 
+# cl <- makePSOCKcluster(10)
+# registerDoParallel(cl)
+# CV_results <- pog_workflow %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc))
+# stopCluster(cl)
+# 
+# bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# final_pog_wf <-
+#   pog_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=rawdata)
+# 
+# 
+# pog_predictions <- final_pog_wf %>%
+#   predict(new_data = test_input, type="prob")
+# 
+# format_and_write(pog_predictions, "pog_preds.csv")
 
 # Binary RF's --------------------------------------------------------------
 
-BRF_mod <- rand_forest(mtry = tune(),
-                      min_n=tune(),
-                      trees=1000) %>%
-  set_engine("ranger") %>%
-  set_mode("classification")
-
-BRF_workflow <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(BRF_mod)
-
-tuning_grid <- grid_regular(mtry(range=c(1,10)),
-                            min_n(),
-                            levels = 4)
-
-folds <- vfold_cv(rawdata, v = 10, repeats=1)
-
-cl <- makePSOCKcluster(10)
-registerDoParallel(cl)
-CV_results <- BRF_workflow %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics=metric_set(roc_auc))
-stopCluster(cl)
-
-bestTune <- CV_results %>%
-  select_best("roc_auc")
-
-final_BRF_wf <-
-  BRF_workflow %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=rawdata)
-
-
-BRF_predictions <- final_BRF_wf %>%
-  predict(new_data = test_input, type="prob")
-
-format_and_write(BRF_predictions, "rf_preds.csv")
+# BRF_mod <- rand_forest(mtry = tune(),
+#                       min_n=tune(),
+#                       trees=1000) %>%
+#   set_engine("ranger") %>%
+#   set_mode("classification")
+# 
+# BRF_workflow <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(BRF_mod)
+# 
+# tuning_grid <- grid_regular(mtry(range=c(1,10)),
+#                             min_n(),
+#                             levels = 4)
+# 
+# folds <- vfold_cv(rawdata, v = 10, repeats=1)
+# 
+# cl <- makePSOCKcluster(10)
+# registerDoParallel(cl)
+# CV_results <- BRF_workflow %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc))
+# stopCluster(cl)
+# 
+# bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# final_BRF_wf <-
+#   BRF_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=rawdata)
+# 
+# 
+# BRF_predictions <- final_BRF_wf %>%
+#   predict(new_data = test_input, type="prob")
+# 
+# format_and_write(BRF_predictions, "rf_preds.csv")
 
 # Naive Bayes -------------------------------------------------------------
 
-nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
-  set_mode("classification") %>%
-  set_engine("naivebayes")
-
-nb_workflow <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(nb_model)
-
-tuning_grid <- grid_regular(Laplace(),
-                            smoothness(),
-                            levels = 4)
-
-folds <- vfold_cv(rawdata, v = 10, repeats=1)
-
-cl <- makePSOCKcluster(10)
-registerDoParallel(cl)
-CV_results <- nb_workflow %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics=metric_set(roc_auc))
-stopCluster(cl)
-
-bestTune <- CV_results %>%
-  select_best("roc_auc")
-
-final_nb_wf <-
-  nb_workflow %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=rawdata)
-
-
-nb_predictions <- final_nb_wf %>%
-  predict(new_data = test_input, type="prob")
-
-format_and_write(nb_predictions, "nb_preds.csv")
+# nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
+#   set_mode("classification") %>%
+#   set_engine("naivebayes")
+# 
+# nb_workflow <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(nb_model)
+# 
+# tuning_grid <- grid_regular(Laplace(),
+#                             smoothness(),
+#                             levels = 4)
+# 
+# folds <- vfold_cv(rawdata, v = 10, repeats=1)
+# 
+# cl <- makePSOCKcluster(10)
+# registerDoParallel(cl)
+# CV_results <- nb_workflow %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc))
+# stopCluster(cl)
+# 
+# bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# final_nb_wf <-
+#   nb_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=rawdata)
+# 
+# 
+# nb_predictions <- final_nb_wf %>%
+#   predict(new_data = test_input, type="prob")
+# 
+# format_and_write(nb_predictions, "nb_preds.csv")
 
 # KNN ---------------------------------------------------------------------
 
-knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
-  set_mode("classification") %>%
-  set_engine("kknn")
-
-knn_workflow <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(knn_model)
-
-tuning_grid <- grid_regular(neighbors(),
-                            levels = 4)
-
-folds <- vfold_cv(rawdata, v = 10, repeats=1)
-
-cl <- makePSOCKcluster(10)
-registerDoParallel(cl)
-CV_results <- knn_workflow %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics=metric_set(roc_auc))
-stopCluster(cl)
-
-bestTune <- CV_results %>%
-  select_best("roc_auc")
-
-final_knn_wf <-
-  knn_workflow %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=rawdata)
-
-
-knn_predictions <- final_knn_wf %>%
-  predict(new_data = test_input, type="prob")
-
-format_and_write(knn_predictions, "knn_preds.csv")
+# knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
+#   set_mode("classification") %>%
+#   set_engine("kknn")
+# 
+# knn_workflow <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(knn_model)
+# 
+# tuning_grid <- grid_regular(neighbors(),
+#                             levels = 4)
+# 
+# folds <- vfold_cv(rawdata, v = 10, repeats=1)
+# 
+# cl <- makePSOCKcluster(10)
+# registerDoParallel(cl)
+# CV_results <- knn_workflow %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc))
+# stopCluster(cl)
+# 
+# bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# final_knn_wf <-
+#   knn_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=rawdata)
+# 
+# 
+# knn_predictions <- final_knn_wf %>%
+#   predict(new_data = test_input, type="prob")
+# 
+# format_and_write(knn_predictions, "knn_preds.csv")
 
 # Support Vector Machines -------------------------------------------------
 
